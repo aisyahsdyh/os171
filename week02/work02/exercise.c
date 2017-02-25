a313729aca2def00b713c7a015972bd2220d1c39
AISYAH SADIYAH - 1606954703 <aisyah.sadiyah@badak.cs.ui.ac.id>
2017-02-25 12:37:48 +0700

    create work02

---
+++ week02/work02/exercise.c
@@ -0,0 +1,17 @@
+/*
+* Name: Aisyah Sadiyah
+* NPM: 1606954703
+* Class: Sistem Informasi Ekstensi 2016
+* Comment: What is this?
+*/
+#define LOOP 4
+#include <stdio.h>
+void main() {
+ int input = 5;
+ int a,b=0;
+ for (a=0; a<LOOP; a++){
+	b=b+input;
+ }
+ printf("%d times %d equal %d\n", input,LOOP,b);
+}
+
